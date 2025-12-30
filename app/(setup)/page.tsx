55e9d30d405209c231cd1931a19af8eb8247895c
dhruvbhardwaj-debug <logicunit64@gmail.com>
2025-12-31 01:44:53 +0530

    uploadthing

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,8 @@
 import {initialProfile} from "@/lib/initial-profile";
 import {db} from "@/lib/db"
 import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
+
 const SetupPage = async () => {
     const profile = await initialProfile();
     const server = await db.server.findFirst({
@@ -9,14 +11,13 @@ const SetupPage = async () => {
                 some:{
                     profileId:profile.id
                 }
-
             }
         }
     })
-    if(server)return redirect(`/servers/${server.id}`);
-  return (
-    <div>create new server</div>
-  )
+    
+    if(server) return redirect(`/servers/${server.id}`);
+    
+    return  <InitialModal/>
 }
 
 export default SetupPage
---
+++ components/file-upload.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { UploadDropzone } from "@/lib/uploadthing";
+
+import "@uploadthing/react/styles.css"
+
+interface FileUploadProps{
+    onChange:(url?:string)=>void;
+    value:string;
+    endpoint:"serverImage" | "messageFile";
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const FileUpload = ({onChange,value,endpoint}:FileUploadProps)=>{
+    return(
+        <div>
+            <UploadDropzone
+            endpoint = {endpoint}
+            onClientUploadComplete={(res)=>{
+                onChange(res?.[0].url);
+            }}
+            onUploadError={(error:Error)=>{
+                console.log(error);
+            }}
+            />
+        </div>
+    ) 
+}
